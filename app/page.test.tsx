0fcf8fb4d5f2f9f8e3da4d14e796074650d94424
Sandeep Kumar Divi <sandeepkumar.divi@cesltd.com>
2023-12-28 20:23:38 +0530

    added the fetch todo list changes with test cases

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,15 +1,52 @@
-import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import fetchMock from 'jest-fetch-mock'
+import { useRouter } from 'next/navigation'
+import { appLabels } from './app-constants'
 import Main from './page'
+import { generateRandomData } from './test-utils/utils'
+
+const mockResponse = generateRandomData(1)
+
+fetchMock.enableMocks()
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
 }))
 
-describe('Main Page', () => {
-  test('main page renders', () => {
-    render(<Main />)
-    const pageElement = screen.getByTestId('main-element')
-    expect(pageElement).toBeVisible()
+describe('Main Page:', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+    const push = jest.fn()
+    const mockRouter = useRouter as jest.Mock
+    mockRouter.mockImplementation(() => ({
+      push,
+    }))
+  })
+
+  it('should render the main page elements', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse))
+    render(await Main())
+    expect(screen.getByTestId('main-element')).toBeInTheDocument()
+    expect(screen.getAllByRole('listitem').length).toBe(mockResponse.length)
+  })
+
+  it('should render the right todo list element', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse))
+    render(await Main())
+    const initialTodoName = mockResponse[0].name
+    const listElements = screen.getAllByRole('listitem')
+    expect(listElements[0].textContent).toBe(initialTodoName)
+  })
+
+  it('should display the error message, when fetch todo fails', async () => {
+    fetchMock.mockRejectOnce()
+    render(await Main())
+    expect(screen.getByText(appLabels.NO_TODO_RESULTS_TEXT)).toBeInTheDocument()
+  })
+
+  it('should call the fetch with valid request url', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse))
+    render(await Main())
+    expect(fetchMock).toHaveBeenCalledWith(`${appLabels.ROOT_URL}/todo-list`, { cache: 'no-store' })
   })
 })
